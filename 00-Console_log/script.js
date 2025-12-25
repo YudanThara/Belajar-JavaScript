a0bcef74149c78e5746f46aec87d6bf5e59f2675
Yudan <yudanprihantara98@gmail.com>
2025-12-25 22:02:39 +0700

    Upload commit pertama belajar JS

---
+++ 00-Console_log/script.js
@@ -0,0 +1,5 @@
+console.log("Hai, namaku Yudan")
+console.log("Belajar JavaScript untuk melengkapi fundamental web development")
+
+let nama = "Yudan"
+console.log("Halo, nama saya " + nama + ". Senang berkenalan denganmu!")
